89ff10dc1ee45663e387e16854bf26a91c9c4ad0
Jack Cameo <jack@klatchit.com>
2013-06-09 11:26:10 -0400

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Ddcrawler::Application.routes.draw do
-  get "static_pages/home"
+  get "users/new"
+
+  # get "static_pages/home"
+  match '/home', to: 'static_pages#home'
+  root to: 'static_pages#home'
 
   get "static_pages/help"
 
