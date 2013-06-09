0d15915eac1058fd531abdae7512e55ff9d928af
Jack Cameo <jack@klatchit.com>
2013-06-09 09:22:24 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ddcrawler::Application.config.secret_token = '30f18be3e69ec342da2e9729a04cc4c13f82834cd91a0bf00c8739036979adcf344fa20bc5ecf917742d8abb3bdaad18738b2d611d0ad3d4d5043b98c5dc7283'
